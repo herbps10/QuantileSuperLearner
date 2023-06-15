790d15cc1840572b29590dc5cddf8431989c87a9
Herb Susmann <herbps10@gmail.com>
2023-06-15 19:01:11 +0200

    Rearranging to set up script for server use

--- R/quantile_tasks.R
+++ R/quantile_tasks.R
@@ -25,58 +25,9 @@ quantile_sl <- function(task, tau) {
   sl_fit
 }
 
-# Read abalone data
-abalone <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data", col_names = c("sex", "length", "diameter", "height", "weight.whole", "weight.shucked", 
-                    "weight.viscera", "weight.shell", "rings"))
-
-winequality <- read_delim("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", delim = ";")
-names(winequality) <- str_replace_all(names(winequality), " ", "")
-
-
-concrete <- read_csv(
-  "data/concrete+compressive+strength/Concrete_Data.csv", 
-  col_names = c("cement", "slag", "flyash", "water", "superplasticizer", "coarse", "fine", "age", "strength"),
-  skip = 1
-)
-
 perovskite <- read_csv(
   "data/combine.csv"
 )
-
-boston_task <- sl3_Task$new(
-  data = MASS::Boston,
-  covariates = c("crim", "zn", "indus", "chas", "nox", "age", "dis", "rad", "tax", "ptratio", "black", "lstat"),
-  outcome = "medv",
-  folds = 5L,
-  outcome_type = "continuous"
-)
-
-abalone_task <- sl3_Task$new(
-  data = abalone,
-  covariates = c("sex", "length", "diameter", "height", "weight.whole", "weight.shucked", "weight.viscera", "weight.shell"),
-  outcome = "rings",
-  folds = 5L,
-  outcome_type = "continuous"
-)
-
-wine_task <- sl3_Task$new(
-  data = winequality,
-  covariates = c("fixedacidity", "volatileacidity", "citricacid", "residualsugar", "chlorides",
-                 "freesulfurdioxide", "totalsulfurdioxide", "density", "pH", "sulphates",
-                 "alcohol"),
-  outcome = "quality",
-  folds = 5L,
-  outcome_type = "continuous"
-)
-
-concrete_task <- sl3_Task$new(
-  data = concrete,
-  covariates = c("cement", "slag", "flyash", "water", "superplasticizer", "coarse", "fine", "age"),
-  outcome = "strength",
-  folds = 5L,
-  outcome_type = "continuous"
-)
-
 
 perovskite_task <- sl3_Task$new(
   data = perovskite,
@@ -86,11 +37,3 @@ perovskite_task <- sl3_Task$new(
   outcome_type = "continuous"
 )
 
-perovskite_task_formation <- sl3_Task$new(
-  data = perovskite,
-  #covariates = setdiff(names(perovskite), c("formula", "Eg", "Ef", "av_rsp_mean", "av_rsp_std")),
-  covariates <- c("z_mean", "z_std"),
-  outcome = "Ef",
-  folds = 5L,
-  outcome_type = "continuous"
-)
